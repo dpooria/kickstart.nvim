412bcf56b64b1a6a6da66a7c549ece364492ff64
Pooria <dpooria75@gmail.com>
2024-09-03 15:01:57 +0200

    isort mypy

--- init.lua
+++ init.lua
@@ -609,6 +609,9 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         pylsp = {},
+        isort = {},
+        mypy = {},
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
