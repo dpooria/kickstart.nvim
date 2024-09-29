a3a67717a2f2f15750e2fdfd8ad081301e2a9e1f
Pooria <dpooria75@gmail.com>
2024-09-29 12:54:37 +0200

    fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,7 +35,7 @@ return {
         -- keywordStyle = { italic = true},
         -- statementStyle = { bold = true },
         -- typeStyle = {},
-        transparent = true, -- do not set background color
+        -- transparent = true, -- do not set background color
         -- dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
         -- terminalColors = true,       -- define vim.g.terminal_color_{0,17}
         -- colors = {                   -- add/modify theme and palette colors
@@ -121,6 +121,17 @@ return {
 
       -- Open parent directory in floating window
       vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+  { 'tpope/vim-fugitive' },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
     end,
   },
 }
--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -99,4 +99,27 @@ vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 -- vim.api.nvim_set_keymap('n', ',.', ':lua vim.cmd("normal! " .. vim.g.last_op)<CR>', { noremap = true, silent = true })
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
+-- search on google
+-- Function to search Google with the visually selected text
+local function get_visual_selection()
+  local s_start = vim.fn.getpos "'<"
+  local s_end = vim.fn.getpos "'>"
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
+function SEARCH_ON_GOOGLE()
+  local query = get_visual_selection():gsub('#', '%%23')
+  local url = 'https://www.google.com/search?q=' .. vim.fn.escape(query, ' ')
+  vim.fn.jobstart({ 'qutebrowser', url }, { detach = true })
+end
+
+vim.keymap.set('v', '<leader>sg', ':<C-u>lua SEARCH_ON_GOOGLE()<CR>', { noremap = true, silent = true })
 return {}
